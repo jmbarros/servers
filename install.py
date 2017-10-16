469840eab3e5ae069b3ea3be98d43d8eed537d10
JMBarros <jmbarros@users.noreply.github.com>
2017-10-16 15:26:06 -0200

    change to beta2

--- install.py
+++ install.py
@@ -43,12 +43,12 @@ f = open(fileout,'w')
 f.write(newdata)
 f.close()
 ##########################################################################
-docker_run("-e LICENSE=accept  -v \"$(pwd)\":/data ibmcom/icp-inception:2.1.0-beta-3 cp -r cluster /data")
+docker_run("-e LICENSE=accept  -v \"$(pwd)\":/data ibmcom/icp-inception:2.1.0-beta-2 cp -r cluster /data")
 
 copy("~/.ssh/id_rsa", "~/cluster/ssh_key")
 copy("~/servers/hosts", "~/cluster/hosts")
 play_book("~/servers/hosts.yml", "~/inventory")
 play_book("~/servers/config_prereq.yml", "~/inventory")
 
-docker_run("-e LICENSE=accept --net=host  -t -v \"$(pwd)/cluster\":/installer/cluster ibmcom/icp-inception:2.1.0-beta-3 install")
+docker_run("-e LICENSE=accept --net=host  -t -v \"$(pwd)/cluster\":/installer/cluster ibmcom/icp-inception:2.1.0-beta-2 install")
 
